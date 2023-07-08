b8d7df0691a2666572ac73ed0b1ed768fad3efb2
jinnarajin <102786826+jinnarajin@users.noreply.github.com>
2023-07-08 17:48:16 +0900

    [add] wordle

---
+++ script.js
@@ -0,0 +1,26 @@
+var 답 ='hello';
+        document.querySelector('button').addEventListener('click',
+        function(){
+            var input=document.querySelectorAll('.input');
+
+            for (i=0;i<5;i++){
+                if (input[i].value==답[i]){
+                    input[i].style.background='green';
+                } else if (답.includes(input[i].value)){
+                    input[i].style.background='yellow';
+                }
+                else{
+                    input[i].style.background='lightgrey';
+                }
+                input[i].classList.remove('input')
+            }
+            var template=`<div>
+                <input class="input">
+                <input class="input">
+                <input class="input">
+                <input class="input">
+                <input class="input">
+            </div>`;
+            document.querySelector('body').insertAdjacentHTML("beforeend",template);
+
+        })
